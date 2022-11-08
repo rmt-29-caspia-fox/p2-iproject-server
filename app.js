afc403267a4205fa0c591aaf8a0eace879114842
irwNd2 <irwandduadua@gmail.com>
2022-11-09 02:52:27 +0800

    Register done

--- app.js
+++ app.js
@@ -21,4 +21,42 @@ connectToDb((err) => {
   }
 });
 
+app.post("/register", async (req, res) => {
+  try {
+    let { name, email, password } = req.body;
+    const userExisted = await db.collection("users").findOne({ email: email });
+    if (userExisted) {
+      throw { name: "email_duplicate" };
+    }
+    if (name === undefined || name == "") {
+      throw { name: "name_required" };
+    }
+    if (email === undefined || email == "") {
+      throw { name: "email_required" };
+    }
+    if (password === undefined || password == "") {
+      throw { name: "password_required" };
+    }
+    password = hashPass(password);
+    const rent = [];
+    const newUser = await db
+      .collection("users")
+      .insertOne({ name, email, password, rent });
+
+    res.status(201).json({ id: newUser.insertedId, email: email });
+  } catch (error) {
+    if (error.name == "name_required") {
+      res.status(400).json({ message: "Name is required" });
+    } else if (error.name == "email_required") {
+      res.status(400).json({ message: "Email is required" });
+    } else if (error.name == "email_duplicate") {
+      res.status(400).json({ message: "Email is already registered" });
+    } else if (error.name == "password_required") {
+      res.status(400).json({ message: "Password is required" });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+});
+
 

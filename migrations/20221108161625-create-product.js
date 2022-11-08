70cdec306774006bed2ca5872aa4a39ef90d67a8
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-11-09 00:27:59 +0800

    feat(product): migration, model, validation for product

---
+++ migrations/20221108161625-create-product.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      imgUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      CategoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Categories',
+          key: 'id'
+        },
+      },
+      UserId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key: 'id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};

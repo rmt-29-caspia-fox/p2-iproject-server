16b7e051f49aaec2a4233cb0857ffcdf4e40699e
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-11-09 11:41:09 +0800

    feat(product): end point product CRUD

---
+++ controllers/productController.js
@@ -0,0 +1,53 @@
+const { Product } = require("../models")
+
+class ProductController {
+  static async getProducts(req, res, next){
+    try {
+      const products = await Product.findAll({
+        order: [['id', 'ASC']]
+      })
+      res.status(200).json(products)
+    } catch (err) {
+      next(err)
+    }
+  }
+  static async addProduct(req, res, next){
+    try {
+      const UserId = req.user.id
+      const {name, description, price, imgUrl, CategoryId} = req.body
+      const product = await Product.create({name, description, price, imgUrl, CategoryId, UserId})
+      res.status(201).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+  static async deleteProduct(req, res, next){
+    try {
+      const {id} = req.params
+      const productFind = await Product.findOne({where:{id}})
+      if(!productFind){
+        throw {name: "product_not_found"}
+      }
+      const product = await Product.destroy({where:{id}})
+      res.status(201).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+  static async updateProduct(req, res, next){
+    try {
+      const {id} = req.params
+      const productFind = await Product.findOne({where:{id}})
+      if(!productFind){
+        throw {name: "product_not_found"}
+      }
+      const {name, description, price, imgUrl, CategoryId} = req.body
+      const product = await Product.update({name, description, price, imgUrl, CategoryId},{where:{id}})
+      res.status(201).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = ProductController
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router()
 const userRouter = require('./users')
 const categoriesRouter = require('./categories')
+const productsRouter = require('./products')
+const authentication = require('../middleware/authentication')
 
 router.get('/test', (req, res) => {
   res.status(200).json({message: "OK"})
 })
-
 router.use('/users', userRouter)
+
+router.use(authentication)
 router.use('/categories', categoriesRouter)
+router.use('/products', productsRouter)
 
 module.exports = router

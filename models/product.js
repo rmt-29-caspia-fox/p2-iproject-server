8b9490c7f0c41b529ede8b7000be81978f8a12c6
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-11-09 10:25:10 +0800

    feat(models): relation each models

--- models/product.js
+++ models/product.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Product.belongsTo(models.Category)
+      Product.belongsTo(models.Users)
     }
   }
   Product.init({

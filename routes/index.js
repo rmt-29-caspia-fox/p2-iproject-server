7b785e097834052dcfa3e0df776c4d6ce816600d
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-11-09 13:30:53 +0800

    feat(routes): add public routes for customer

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ const router = express.Router()
 const userRouter = require('./users')
 const categoriesRouter = require('./categories')
 const shippingCostRouter = require('./shippingCost')
+const publicRouter = require('./public')
 const productsRouter = require('./products')
 const authentication = require('../middleware/authentication')
 
 router.get('/test', (req, res) => {
   res.status(200).json({message: "OK"})
 })
-router.use('/users', userRouter)
+
+
+router.use('/public', publicRouter)
 router.use('/shipping-cost', shippingCostRouter)
 
 router.use(authentication)
+router.use('/users', userRouter)
 router.use('/categories', categoriesRouter)
 router.use('/products', productsRouter)
 

1f7ccd421606508320c4285b974b0d2b3b32a05a
ekadk <ekadharmakusuma3@gmail.com>
2022-11-09 16:18:20 +0800

    feat: user register-login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const librarianRouter = require('./librarians');
+const userRouter = require('./users');
 const authentication = require('../middlewares/authentication');
 
 router.use('/librarians', librarianRouter)
+router.use('/user', userRouter)
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,7 @@
-const { comparePassword } = require('../helpers/hashPassword');
-const { User } = require('../models');
-const { encodeToken } = require('../helpers/jwt');
+const UserController = require('../controller/users');
+const router = require('express').Router();
 
-module.exports = class UserController {
-  static async register(req, res, next) {
-    try {
-      const { firstName, lastName, email, password, phoneNumber, role } = req.body
-      const user = await User.create({ firstName, lastName, email, password, phoneNumber, role })
-      res.status(201).json({ id: user.id, email: user.email })
-    } catch (error) {
-      next(error)
-    }
-  }
-  static async login(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      if(!email) {
-        throw { name: "email_required" };
-      }
-      if(!password) {
-        throw { name: "password_required" };
-      }
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
 
-      const user = await User.findOne({ where: { email } });
-
-
-      if (!user) {
-        throw { name: "invalid_username_or_password" };
-      }
-
-      const checkPassword = comparePassword(password, user.password);
-      if (!checkPassword) {
-        throw { name: "invalid_username_or_password" };
-      }
-
-      const payload = { id: user.id };
-      const access_token = encodeToken(payload);
-
-      res
-        .status(200)
-        .json({ access_token, email: user.email, role: user.role });
-    } catch (error) {
-      next(error);
-    }
-  }
-}
+module.exports = router

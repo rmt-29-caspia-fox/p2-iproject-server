096c3d14b59c32692796ded36ad2b4fd99d882d0
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-09 10:44:26 +0700

    nodemailer done

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,4 +1,5 @@
-const { Customer,WaitingList } = require("../models");
+const { Customer, WaitingList } = require("../models");
+const nodemailer = require("nodemailer");
 
 class CustomerController {
   static async register(req, res, next) {
@@ -7,17 +8,53 @@ class CustomerController {
       const data = await Customer.create({ name, email });
       res.status(201).json({ id: data.id, email: data.email });
     } catch (err) {
-        next(err);
+      next(err);
     }
   }
-  static async addWaitingList(req,res,next){
+  static async addWaitingList(req, res, next) {
     try {
-        const CustomerId = req.params.customerid
-        const { brand, name, status, licenseNumber, service} = req.body
-        const data = await WaitingList.create({brand,name,status,licenseNumber,service, CustomerId})
-        res.status(201).json(data);
+      const CustomerId = req.params.customerid;
+      const { brand, name, status, licenseNumber, service } = req.body;
+      const data = await WaitingList.create({
+        brand,
+        name,
+        status,
+        licenseNumber,
+        service,
+        CustomerId,
+      });
+      res.status(201).json(data);
     } catch (err) {
-        next(err)
+      next(err);
+    }
+  }
+  static async mailer(req, res, next) {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.GMAIL_EMAIL,
+          pass: process.env.GMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.GMAIL_EMAIL,
+        to: process.env.GMAIL_EMAIL,
+        subject: "Subject",
+        text: "Email content",
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (!error) {
+          throw {name: name}
+        } else {
+          console.log("Email sent: " + info.response);
+          // do something useful
+        }
+      });
+    } catch (err) {
+      next(err);
     }
   }
 }
--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,9 @@
 const CustomerController = require('../controllers/customerController')
 const router = require('express').Router()
 
-//=> /customer/
+//=> /customers/....
 router.post('/register',CustomerController.register)
 router.post('/waitinglists/:customerid',CustomerController.addWaitingList)
+router.post('/mailer', CustomerController.mailer)
 
 module.exports = router

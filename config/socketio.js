f2738263950ef0301c225198ee81ee793f23d7ac
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-10 08:15:30 +0700

    add key.env, and google sign in

--- config/socketio.js
+++ config/socketio.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(3030, {
   cors: {
-    origin: ["http://localhost:8080"],
+    origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST"],
     credentials: true,
     transports: ["websocket", "polling"],

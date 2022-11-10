73fe989fd87fd645eb7a3bf0a5876eb982da6de5
irwNd2 <irwandduadua@gmail.com>
2022-11-10 08:01:35 +0800

    proses midtrans dan nodemailer done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3000;
 const { hashPass, comparePass } = require("./helpers/bcrypt");
 const { encodeToken, decodeToken } = require("./helpers/jwt");
 const { ObjectId } = require("mongodb");
+const midtransClient = require("midtrans-client");
+const SERVER_KEY = 'SB-Mid-server-qqmfBfQpxzgn-GlHq_SMgLWR';
+const CLIENT_KEY = 'SB-Mid-client-FYlYYii3GW9F6Nre';
+const nodemailer = require("nodemailer");
 
 app.use(cors());
 app.use(express.json());
@@ -110,22 +114,23 @@ app.get("/vehicles", (req, res) => {
     });
 });
 
-
-app.get("/vehicles/:id", async(req, res) => {
+app.get("/vehicles/:id", async (req, res) => {
   try {
-    const car = await db.collection("vehicles").findOne({_id: ObjectId(req.params.id)})
-    if(!car) {
-      throw { name: 'notFound'}
+    const car = await db
+      .collection("vehicles")
+      .findOne({ _id: ObjectId(req.params.id) });
+    if (!car) {
+      throw { name: "notFound" };
     }
-    res.status(200).json(car)
+    res.status(200).json(car);
   } catch (error) {
-    if(error.name == 'notFound') {
-      res.status(404).json({ message: 'Vehicle is not found'})
+    if (error.name == "notFound") {
+      res.status(404).json({ message: "Vehicle is not found" });
     } else {
-      res.status(500).json({ message: 'Internal server error'})
+      res.status(500).json({ message: "Internal server error" });
     }
   }
-})
+});
 
 //
 
@@ -239,3 +244,131 @@ app.get("/myrent", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+app.get("/mydetail", async(req, res) => {
+  try {
+    const user = await db.collection("users").findOne({ _id: ObjectId(req.user.id)})
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error'})
+  }
+})
+
+app.get("/vehicles/rent/pay/detail/:vehicleId", async (req, res) => {
+  try {
+    const user = await db
+      .collection("users")
+      .findOne({ _id: ObjectId(req.user.id) });
+
+    const toPay = user.rent.filter(el => el.vehicleId == String(req.params.vehicleId) && el.paymentStatus === false)
+    res.status(200).json({user: user.name, email: user.email, toPay})
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error'})
+  }
+})
+
+// app.get("/payment/midtrans/:id", async (req, res) => {
+//   try {
+//     const user = await db
+//       .collection("users")
+//       .findOne({ _id: ObjectId(req.user.id) });
+
+//     let snap = new midtransClient.Snap({
+//       isProduction: false,
+//       serverKey: "SB-Mid-server-qqmfBfQpxzgn-GlHq_SMgLWR",
+//     });
+
+//     let parameter = {
+//       transaction_details: {
+//         order_id: "ORDERID-" + user._id,
+//         gross_amount: +user.rent[+req.params.id].totalPrice,
+//       },
+//       credit_card: {
+//         secure: true,
+//       },
+//       customer_details: {
+//         name: user.name,
+//         email: user.email,
+//       },
+//     };
+
+//     snap.createTransaction(parameter).then((transaction) => {
+//       // transaction token
+//       let transactionToken = transaction.token;
+//       console.log("transactionToken:", transactionToken);
+//       res.status(200).json({transactionToken})
+//     });
+//   } catch (error) {
+//     console.log(error)
+//     res.status(500).json({ message: 'Internal server error'})
+//   }
+// });
+
+app.get("/payment/:id/checkout", async (req, res) => {
+  try {
+    let snap = new midtransClient.Snap({
+      isProduction : false,
+      serverKey : SERVER_KEY,
+      clientKey : CLIENT_KEY
+    });
+    const user = await db
+      .collection("users")
+      .findOne({ _id: ObjectId(req.user.id) });
+
+      let parameter = {
+        "transaction_details": {
+          "order_id": "order-id-"+Math.round((new Date()).getTime() / 1000) + user._id,
+          "gross_amount": +user.rent[+req.params.id - 1].totalPrice,
+        }, "credit_card":{
+          "secure" : true
+        }
+      };
+
+      const transactionToken = await snap.createTransactionToken(parameter)
+      res.status(200).json({ token: transactionToken, clientKey: snap.apiConfig.clientKey})
+  } catch (error) {
+    console.log(error)
+    if(error.name == 'notFound') {
+      res.status(404).json({ message: 'Transaction is not found'})
+    } else {
+      res.status(500).json({ message: 'Internal server error'})
+    }
+  }
+})
+
+app.post("/mailer", async(req, res) => {
+  try {
+    const { email, url, qrcode } = req.body
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'irwand.syah@gmail.com', // generated ethereal user
+        pass: 'pynqzljbycfoplld', // generated ethereal password
+      },
+    });
+  
+    // send mail with defined transport object
+     await transporter.sendMail({
+      from: '"RentalKita" <payment@rentalkita.com>', // sender address
+      to: email, // list of receivers
+      subject: "Payment issued from your order at RentalKita", // Subject line
+      text: "Thank you for your order at RentalKita. To finish your order, please complete the payment by clicking or scanning qr code below.", // plain text body
+      attachments: [{
+        filename: 'picture.png',
+        path: qrcode,
+        cid: 'qrcode2022'
+    }],
+      html: `<p>Thank you for your order at RentalKita. To finish your order, please complete the payment by clicking or scanning qr code below. You can just ignore this if you already completed the payment.</p><br>
+      <a href="${url}">Finish your payment here</a><br>
+      <p>Or</p><br>
+      <img src="cid:qrcode2022">`,
+    });
+
+    res.status(200).json({ message: 'Email sent'})
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error'})
+  }
+})
